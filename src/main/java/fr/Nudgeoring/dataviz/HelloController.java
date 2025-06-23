9adb2060feaaaa3725f35a6e0af3ad52f2357db8
Dric04 <cedric.trevien@gmail.com>
2025-06-23 21:58:22 +0200

    Rajout du Docker

---
+++ src/main/java/fr/Nudgeoring/dataviz/HelloController.java
@@ -0,0 +1,13 @@
+package fr.Nudgeoring.dataviz;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Salut Nudgios !";
+    }
+}
---
+++ src/main/java/fr/Nudgeoring/dataviz/HelloNudgienApplication.java
@@ -0,0 +1,13 @@
+package fr.Nudgeoring.dataviz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloNudgienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloNudgienApplication.class, args);
+	}
+
+}
